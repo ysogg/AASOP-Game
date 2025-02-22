f3f21dca9d6a33f699bea737a2615eee0ea2d762
Alexander <alexander.arnkvarn@gmail.com>
2025-02-22 16:13:12 -0500

    added a banger

--- audio/audio_manager.gd
+++ audio/audio_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
 @onready var main_menu: AudioStreamPlayer2D = $Music/MainMenu
+@onready var gameplay: AudioStreamPlayer2D = $Music/Gameplay
 
-@export var mute: bool = false
+@export var mute_all: bool = false
+@export var mute_music: bool = false
 
 func play_main_menu() -> void:
-	if not mute:
+	if not mute_all and not mute_music:
 		main_menu.play()
+
+func play_gameplay() -> void:
+	if not mute_all and not mute_music:
+		main_menu.stop()
+		gameplay.play()
